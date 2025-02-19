50f3313632d936d8d4f0b4430dd44f1ae5c62a76
yufeng <321353225@qq.com>
2025-02-20 01:28:55 +0800

    update & add rust lsp config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,17 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.g.rustaceanvim = {
+  server = {
+    load_vscode_settings = true,
+    default_settings = {
+      -- rust-analyzer language server configuration
+      ["rust-analyzer"] = {
+        check = {
+          allTargets = false,
+        },
+      },
+    },
+  },
+}
