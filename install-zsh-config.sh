35a347f9755b6c8ed5149bbeb0820f92377eea78
yfblock <321353225@qq.com>
2025-10-07 01:01:33 +0800

    feat: update dnf plugins comp

--- install-zsh-config.sh
+++ install-zsh-config.sh
@@ -7,6 +7,6 @@ for rcfile in $CURPATH/zprezto/runcoms/^README.md(.N); do
   ln -sf "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
-[ ! -f "zsh-config/_dnf" ] && wget https://raw.githubusercontent.com/zsh-users/zsh/master/Completion/Redhat/Command/_dnf -O zsh-config/_dnf
-[ ! -f "zsh-config/_dnf5" ] && wget https://raw.githubusercontent.com/zsh-users/zsh/master/Completion/Redhat/Command/_dnf5 -O zsh-config/_dnf5
+[ ! -f "~/dots/zsh-config/_dnf" ] && wget https://raw.githubusercontent.com/zsh-users/zsh/master/Completion/Redhat/Command/_dnf -O zsh-config/_dnf
+[ ! -f "~/dots/zsh-config/_dnf5" ] && wget https://raw.githubusercontent.com/zsh-users/zsh/master/Completion/Redhat/Command/_dnf5 -O zsh-config/_dnf5
 
